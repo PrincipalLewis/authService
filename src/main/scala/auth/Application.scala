4d633d1a4b2247c5a3b9fa2531ef90654438f951
evgeniy <evgeniy@debian>
2016-08-03 23:16:22 +0300

    last

--- src/main/scala/auth/Application.scala
+++ src/main/scala/auth/Application.scala
@@ -5,14 +5,32 @@ import com.twitter.finagle.http
 import com.twitter.util.{Await, Future}
 import scala.collection.concurrent.TrieMap
 import java.sql.{Connection, DriverManager, ResultSet};
+import scala.util.control.Breaks._
+import scalikejdbc._
 import java.nio.charset.Charset
 
 object Application extends App {
+  classOf[org.postgresql.Driver]
+  val con_st = "jdbc:postgresql://172.17.0.3:5432/postgres?user=postgres&password=111"
+
+  def sti(list: String): Int = {
+    var res = Math.pow(10,(list.length)-1).toInt
+    var r = 0
+    list.foreach(x => {
+      r = r + res * (x.toInt - 48)
+      res = res/10
+    })
+    r
+  }
+
   val service = new Service[http.Request, http.Response] {
     val kv = TrieMap.empty[String,String]
 
+
+
     def apply(req: http.Request): Future[http.Response] = {
       Future {
+        val conn = DriverManager.getConnection(con_st)
         val key = req.uri
         val value = req.contentString
         val resp = {
@@ -30,7 +48,24 @@ object Application extends App {
           }
           case http.Method.Post =>
             {
-              kv.update(key,value.toString)
+              try {
+                val stm = conn.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY)
+
+                val rs = stm.executeQuery("SELECT * from acc")
+                breakable {
+                  while (rs.next) {
+                    if (rs.getString("login") == key && rs.getString("pass") == value.toString) {
+                      val prep = conn.prepareStatement("UPDATE ACC SET val = val + 1")
+                      prep.executeUpdate()
+
+                      println(sti(rs.getString("val"))+1)
+                      break
+                    }
+                  }
+                }
+              } finally {
+                conn.close()
+              }
             }
         }
         resp
@@ -39,6 +74,8 @@ object Application extends App {
   }
 
   val server = Http.serve("127.0.0.1:8885", service)
+
+
 
 
   Await.ready(server)
--- src/main/scala/auth/Connect.scala
+++ src/main/scala/auth/Connect.scala
@@ -8,6 +8,17 @@ import scalikejdbc._
 
 object Connect {
   classOf[org.postgresql.Driver]
-  val con_st = "jdbc:postgresql://172.17.0.2:5432/postgre?user=DB_USER"
-  val conn = DriverManager.getConnection(con_str)
+  val con_st = "jdbc:postgresql://172.17.0.3:5432/pgs_w?user=postgres&password=111"
+  val conn = DriverManager.getConnection(con_st)
+  try {
+    val stm = conn.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY)
+
+    val rs = stm.executeQuery("SELECT * from acc")
+
+    while(rs.next) {
+      println(rs.getString("quote"))
+    }
+  } finally {
+    conn.close()
+  }
 }
